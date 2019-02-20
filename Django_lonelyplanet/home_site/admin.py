321166f84b5c0637321f93e03e2d634c19394183
mohamed elmansy <eng.mohamedelmansy94@gmail.com>
2019-02-20 03:45:06 +0200

    add hotels to admin

--- Django_lonelyplanet/home_site/admin.py
+++ Django_lonelyplanet/home_site/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Country ,City,Sights
+from .models import Country ,City,Sights,Hotels
 # Register your models here.
 
 admin.site.register(City)
 admin.site.register(Country)
 admin.site.register(Sights)
+admin.site.register(Hotels)
