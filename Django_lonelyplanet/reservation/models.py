08230b34ae0e8f65d19aa6259daab72d774d9d34
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-02-20 12:05:08 +0200

    added user id into hotel (not working yet)

--- Django_lonelyplanet/reservation/models.py
+++ Django_lonelyplanet/reservation/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from home_site.models import Country,City,Sights,Hotels
+from django.contrib.auth.models import User
 import datetime
 
 private_storage = FileSystemStorage(location=settings.STATIC_URL)
@@ -29,6 +30,7 @@ class hotel(models.Model):
     from_date = models.DateField(("Date"))
     to_date = models.DateField(("Date"))
     person_number = models.IntegerField(default="1")
+    user=models.ForeignKey(User)
 
 
     def __str__(self):
--- Django_lonelyplanet/reservation/views.py
+++ Django_lonelyplanet/reservation/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import CarForm,HotelForm
-from .models import car,hotel
+from .models import car,hotel,User
+from django.contrib.auth.models import User
 from home_site.models import Country,City,Sights,Hotels
 
 
@@ -28,10 +29,10 @@ def newhotel(request,city_id):
     hotel_form.fields['hotels'].empty_label = "Select your hotel"
     hotel_form.fields['person_number'].empty_label = "Number of persons"
     if request.method == "POST":
+        hotel.objects.create(user=request.POST['id'])
         hotel_form = HotelForm(request.POST)
         if hotel_form.is_valid():
-            hotel_form.save()
-            return HttpResponseRedirect(request.path_info)
-
+                hotel_form.save()
+                return HttpResponseRedirect(request.path_info)
     context = {"form": hotel_form}
     return render(request, "hotel/hotel_res.html", context)
